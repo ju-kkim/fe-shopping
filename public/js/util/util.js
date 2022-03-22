d5a3422b0caefbed0bc1c96bfb4a6b57fef3c29c
ju-kkim <wndus8198@naver.com>
2022-03-22 21:54:23 +0900

    feat: classList.toggle 함수

--- public/js/util/util.js
+++ public/js/util/util.js
@@ -2,3 +2,7 @@ export function getData(path) {
   const data = fetch(`http://localhost:3000/${path}`);
   return data.then((response) => response.json());
 }
+
+export function toggleClass(target, className) {
+  target.classList.toggle(className);
+}
